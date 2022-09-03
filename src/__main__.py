b7580a53ed02c675672a71724d0ac04cfebfea14
Triyan1053 <61227305+Triyan1053@users.noreply.github.com>
2022-09-03 23:42:18 +0530

    fix precommit workflow

--- src/__main__.py
+++
@@ -1,2 +0,0 @@
-# If you are using PyCharm as your ide be sure to run `pre-commit run --all-files` before commiting
-# Because PyCharm dosen't have a Pre-Commit hook.
---
+++ src/main.py
@@ -0,0 +1,3 @@
+# If you are using PyCharm as your ide be sure to run `pre-commit run --all-files` before commiting
+# Because PyCharm dosen't have a Pre-Commit hook.
+print("Hello World")
